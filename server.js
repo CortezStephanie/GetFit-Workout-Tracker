a9118168c7407d40b295326712a9e20d9c2fc5bd
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-11-14 04:38:45 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", 
+{
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useCreateIndex: true,
+   useFindAndModify: false 
+  }
+  );
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
