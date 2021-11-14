cba88daa3aa9131201558baee4e6a6f6c24c5c9d
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-11-14 02:08:57 -0600

    finished everything seems to run correctly

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,73 @@
 const router = require("express").Router();
-const workOuts = require("../models/workouts.js");
+const Workouts = require("../models/workouts.js");
+const mongoose = require("mongoose");
 
 router.get("/api/workouts", (req, res) => {
-    Workouts.find({})
-      .sort({ day: 1 })
-      .then(dbWorkouts => {
-        res.json(dbWorkouts);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-
-router.get("/api/workouts/range", (req, res) => {
-    
+    Workouts.aggregate([{
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }])
+        .sort({
+            day: 1
+        })
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
-  router.post("/api/workouts/bulk", ({ body }, res) => {
-    Workouts.insertMany(body)
-      .then(dbWorkouts => {
-        res.json(dbWorkouts);
-      })
-      .catch(err => {
+router.put("/api/workouts/:id", async (req, res) => {
+    try {
+        const workouts = await Workouts.updateOne({
+            _id: mongoose.Types.ObjectId(req.params.id)
+        }, {
+            $push: {
+                "exercises": req.body
+            }
+        }, {
+            new: true
+        });
+        res.json(workouts);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+router.post("/api/workouts", (req, res) => {
+    Workouts.create(req.body)
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+    try {
+        const workoutRange = await Workouts.aggregate(
+                [{
+                    $addFields: {
+                        totalDuration: {
+                            $sum: "$exercises.duration"
+                        }
+                    }
+                }]
+            )
+            .sort({
+                day: -1
+            }).limit(7)
+        res.json(workoutRange);
+    } catch (err) {
+        console.log(err);
         res.status(400).json(err);
-      });
-  });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const routes = require("./routes/api");
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,9 +15,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutsdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/exercise', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/exercise.html'))
+);
+
+app.get('/stats', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/stats.html'))
+);
 /*
 app.post("/submit", ({ body }, res) => {
   Workouts.create(body)
